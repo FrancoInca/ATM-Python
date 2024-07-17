e3f68c819ffa2c458eb82108be8095c77dc6dacd
Franco Inca <francoinca1604@gmail.com>
2024-07-17 16:13:49 -0500

    feat(ATM): Changed position of message and added admin account

--- ATMStart.py
+++ ATMStart.py
@@ -4,15 +4,13 @@ import Login
 import SignIn
 import database
 
-#Welcome panel:
-
-print("\n--------------------")
-print("¡Bienvenido/a a ATM!")
-print("--------------------\n")
-
 #Option Selector:
 
 def welcomeScreen():
+  #Welcome panel:
+  print("\n--------------------")
+  print("¡Bienvenido/a a ATM!")
+  print("--------------------\n")
   try:
     option = int(input("¿Qué acción desea realizar?:\n1) Crear cuenta \n2) Acceder con tarjeta \n3) Acceder sin tarjeta\n0) Salir\n\nIndique el numero de su opción: "))
     if option == 0:
@@ -35,4 +33,6 @@ def welcomeScreen():
     print("---------------------------------------------------------\n")
     return welcomeScreen()
 
+admin = database.User("Admin",12345678, 1234)
+database.users.append(admin)
 welcomeScreen()
