62b54628498303d8f2e55339d0b8ab14a2531432
Franco Inca <francoinca1604@gmail.com>
2024-07-17 14:25:00 -0500

    feat(signIn): Added basic sign in form

---
+++ SignIn.py
@@ -0,0 +1,34 @@
+#Imports
+import database
+
+# Creating the function:
+
+def start(exitFunction):
+  try:
+    name = input("Introduzca su nombre completo: ")
+    nationalID = int(input("Introduzca su DNI: "))
+    pin = validatePin()
+    newUser = database.User(name, nationalID, pin)
+    database.users.append(newUser)
+    print(f"\nUsuario creado con éxito: Bienvenido/a {name}\nSu tarjeta es: {newUser.cardNumber}\nVuelva a iniciar sesión por favor.\n")
+    exitFunction()
+  except:
+    print("Hubo un error inesperado.")
+    return start(exitFunction)
+
+def validatePin():
+  try:
+    pin = int(input("Introduzca su clave de 4 dígitos de seguridad:"))
+    if pin < 1000 or pin > 9999:
+      print("Debe ser de 4 dígitos")
+      return validatePin()
+    
+    pinTest = int(input("Vuelva a introducir su pin de seguridad: "))
+    if pin == pinTest:
+      print("¡Clave de seguridad añadida exitosamente!")
+      return pin
+    print("Las claves no coinciden.\nInténtelo nuevamente")
+    return validatePin
+  except:
+    print("Ha ocurrido un error, introduzca correctamente su pin.")
+    return validatePin()
